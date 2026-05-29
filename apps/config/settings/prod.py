2c899fa8b130a8bccabdd0d356bcaecc4a9527f0
xusniddinovk1 <kxprogrammer1@gmail.com>
2026-05-29 10:13:01 +0500

    setup files

---
+++ apps/config/settings/prod.py
@@ -0,0 +1,12 @@
+from .base import *
+
+DEBUG = True
+ALLOWED_HOSTS = ["*"]
+
+# DATABASES = {
+#     "default": dj_database_url.parse(
+#         os.environ["DATABASE_URL"],
+#         conn_max_age=600,
+#         ssl_require=True,
+#     )
+# }
